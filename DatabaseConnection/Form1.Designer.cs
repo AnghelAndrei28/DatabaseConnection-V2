f5cf455d96052d6fdee9b83d2d84750131619cac
andrei nfs <andreinfs@yahoo.com>
2019-01-18 19:06:44 +0200

    Initial Commit

--- DatabaseConnection/Form1.Designer.cs
+++ DatabaseConnection/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StudentsDGV = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StudentsDGV
+            // 
+            this.StudentsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentsDGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name});
+            this.StudentsDGV.Location = new System.Drawing.Point(12, 12);
+            this.StudentsDGV.Name = "StudentsDGV";
+            this.StudentsDGV.Size = new System.Drawing.Size(387, 202);
+            this.StudentsDGV.TabIndex = 1;
+            // 
+            // Name
+            // 
+            this.Name.DataPropertyName = "name";
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(425, 266);
+            this.Controls.Add(this.StudentsDGV);
+            this.Controls.Add(this.button1);
+            //this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsDGV)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView StudentsDGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
     }
 }
 
--- DatabaseConnection/Form1.cs
+++ DatabaseConnection/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DatabaseConnection
 {
     public partial class Form1 : Form
     {
+        String connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Students;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; // din prprietatile "localdb"
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            String sqlCommand = "SELECT name,note FROM GRADES";  //selecteaza tot din baza de date
+            using (connection)
+            {
+                connection.Open();
+                SqlDataAdapter command = new SqlDataAdapter(sqlCommand,connection);
+
+                DataTable dt = new DataTable();
+                command.Fill(dt);
+                StudentsDGV.AutoGenerateColumns = false;
+                StudentsDGV.DataSource = dt;
+            }
+        }
+
+       
     }
 }
